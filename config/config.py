b5c67d99da6595db9f36a5381690722f260826bb
Romaan313 <108215028+Romaan313@users.noreply.github.com>
2023-08-22 13:03:38 +0530

    Yummi Music

--- config/config.py
+++ config/config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "5278339583").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Revenger2901/NyraMusic")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Revenger2901/Musicbot")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Mine_Bots")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/Silent_robo_11")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/II_DPZ_II")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ii_U_F_C_ii")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
